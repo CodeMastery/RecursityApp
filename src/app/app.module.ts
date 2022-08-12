4cf8ca38649280c751def756b94a25f6a02d5717
CodeMastery <chadnashtkd@gmail.com>
2022-08-12 14:34:52 -0700

    1 file modified, 15 added, 12 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import {NodemapComponent} from '../components/nodemap/nodemap.component'
-import {RendererComponent} from '../components/renderer/renderer.component'
-import {NodemeshComponent} from '../components/nodemesh/nodemesh.component';
-import { ToolsService } from '../services/tools.service';
+import {NodemapComponent} from './Mind-Map/components/nodemap/nodemap.component'
+import {RendererComponent} from './Mind-Map/components/renderer/renderer.component'
+import {NodemeshComponent} from './Mind-Map/components/nodemesh/nodemesh.component';
+import { ToolsService } from './Mind-Map/services/tools.service';
+import { appRoutingModule } from './app.routing';
 
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
+  imports:      [ BrowserModule, FormsModule, appRoutingModule ],
   declarations: [ AppComponent, NodemeshComponent, RendererComponent, NodemapComponent ],
   bootstrap:    [ AppComponent ],
   providers:    [ToolsService]
