67ac97957a00c1859d9c658b0ea2547de66cc1e2
CodeMastery <chadnashtkd@gmail.com>
2022-08-12 14:37:41 -0700

    1 file modified, 2 deleted

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,9 +1,9 @@
 import { Routes, RouterModule } from '@angular/router';
 
-import { HomeComponent } from './Home/home/home.component';
+
 
 const routes: Routes = [
-    { path: '', component: HomeComponent },
+    { },
 
     // otherwise redirect to home
     { path: '**', redirectTo: '' }
